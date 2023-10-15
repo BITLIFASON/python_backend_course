ce0acd4ac437c2daf93e40c6d3cecf7bb021ebc0
BITLIFASON <vladov.ru@gmail.com>
2023-10-15 12:23:44 +0300

    add info about main service

--- hw_3/README.md
+++ hw_3/README.md
@@ -6,8 +6,9 @@ To speed up the launch of the test and the application, you need to run:
 3) change the source of models via variable "pt_save_directory" from the comment;
 4) change the timer to unit_test.sh.
 
-Run instructions:
+The main service is available at http://localhost:8001/analysis/, it takes the query parameter "text".
 
+Automatic run instructions:
 ```bash
 # Run app
 run_app.sh
--- hw_3/main_service/main.py
+++ hw_3/main_service/main.py
@@ -13,6 +13,15 @@ app = FastAPI(
 
 @app.get("/analysis/")
 async def get_analysis(text: str) -> str:
+    """
+    Analyzes topic and the sentiment of text
+
+    - Args:
+        - data : input text
+
+    - Returns:
+        - str : text description
+    """
     sent_text = requests.post('http://sentiment_service:8002/sentiment/', json={"text": text}).json() # http://sentiment_service:8002/sentiment/ http://127.0.0.1:8002/sentiment/
     class_text = requests.post('http://classification_service:8003/classification/', json={"text": text}).json() # http://classification_service:8003/classification/ http://127.0.0.1:8003/classification/
     return f'This {sent_text} text about {class_text}'
